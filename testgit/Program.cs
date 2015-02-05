26c57db8466ebca7974338c9a570f6111f974d24
ziyezizy7 <zengyan7@hotmail.com>
2015-02-05 16:14:58 +0800

    1

---
+++ testgit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testgit
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
