bb1f007c5c35248d213bb8ccb2679f69b3e7e0bd
yazeng <yazeng@microsoft.com>
2015-09-16 10:21:07 +0800

    Initial commit

---
+++ testgit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testgit
+{
+    class Program
+    {
+	/// 20150825
+        /// <summary>
+        /// Add comment
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testGit");
+            Console.WriteLine("Write a new line");
+
+        }
+    }
+}
